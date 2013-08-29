c9efdb840cc83c2b4d68b56a625e923bf249e7c3
Thomas Ardal <thomasardal@gmail.com>
2013-08-29 13:56:11 +0200

    Moved IWebClientExtensions to its own file

--- Elmah.Io/IWebClient.cs
+++ Elmah.Io/IWebClient.cs
@@ -8,29 +8,4 @@ namespace Elmah.Io
         T Post<T>(WebHeaderCollection headers, Uri address, string data, Func<WebHeaderCollection, string, T> resultor);
         T Get<T>(WebHeaderCollection headers, Uri address, Func<WebHeaderCollection, string, T> resultor);
     }
-                                        // ReSharper disable InconsistentNaming
-    static class IWebClientExtensions   // ReSharper restore InconsistentNaming
-    {
-        public static string Post(this IWebClient client, Uri address, string data)
-        {
-            return Post(client, null, address, data);
-        }
-
-        public static string Post(this IWebClient client, WebHeaderCollection headers, Uri address, string data)
-        {
-            if (client == null) throw new ArgumentNullException("client");
-            return client.Post(headers, address, data, (_, r) => r);
-        }
-
-        public static string Get(this IWebClient client, Uri address)
-        {
-            return Get(client, null, address);
-        }
-
-        public static string Get(this IWebClient client, WebHeaderCollection headers, Uri address)
-        {
-            if (client == null) throw new ArgumentNullException("client");
-            return client.Get(headers, address, (_, r) => r);
-        }
-    }
 }
---
+++ Elmah.Io/IWebClientExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Elmah.Io
+{
+    static class IWebClientExtensions
+    {
+        public static string Post(this IWebClient client, Uri address, string data)
+        {
+            return Post(client, null, address, data);
+        }
+
+        public static string Post(this IWebClient client, WebHeaderCollection headers, Uri address, string data)
+        {
+            if (client == null) throw new ArgumentNullException("client");
+            return client.Post(headers, address, data, (_, r) => r);
+        }
+
+        public static string Get(this IWebClient client, Uri address)
+        {
+            return Get(client, null, address);
+        }
+
+        public static string Get(this IWebClient client, WebHeaderCollection headers, Uri address)
+        {
+            if (client == null) throw new ArgumentNullException("client");
+            return client.Get(headers, address, (_, r) => r);
+        }
+    }
+}
