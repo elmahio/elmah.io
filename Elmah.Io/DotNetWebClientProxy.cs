e89d65ba4a3004f16aa23258828ce0fbed487595
Atif Aziz <code@raboof.com>
2013-09-01 17:47:21 +0200

    Reimplementation of DotNetWebClientProxy using WebRequest/Response for web app support

--- Elmah.Io/DotNetWebClientProxy.cs
+++ Elmah.Io/DotNetWebClientProxy.cs
@@ -1,19 +1,72 @@
 ﻿using System;
-using System.Collections.Specialized;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
+using System.Net.Mime;
+using System.Text;
 using System.Threading.Tasks;
+using Mannex.Net;
+using Mannex.Net.Mime;
+using Mannex.Threading.Tasks;
 
 namespace Elmah.Io
 {
     internal class DotNetWebClientProxy : IWebClient
     {
-        public Task<T> Post<T>(WebHeaderCollection headers, Uri address, string data, Func<WebHeaderCollection, string, T> resultor)
+        public Task<T> Post<T>(WebHeaderCollection headers, Uri address, string data,
+            Func<WebHeaderCollection, string, T> resultor)
         {
             if (address == null) throw new ArgumentNullException("address");
+            if (data == null) throw new ArgumentNullException("data");
             if (resultor == null) throw new ArgumentNullException("resultor");
-            
-            return Transact(headers, webClient => webClient.UploadStringAsTask(address, data), resultor);
+
+            var request = (HttpWebRequest) WebRequest.Create(address);
+            request.Method = "POST";
+
+            if (headers != null)
+            {
+                // Some headers like Content-Type cannot be added directly
+                // and therefore must be treated specially and go over 
+                // the corresponding property on the HttpWebRequest object.
+
+                var headerz = new WebHeaderCollection {headers};
+                var contentType = headerz[HttpRequestHeader.ContentType];
+                if (contentType != null)
+                {
+                    request.ContentType = contentType;
+                    headerz.Remove(HttpRequestHeader.ContentType);
+                }
+
+                request.Headers.Add(headerz);
+            }
+
+            var encoding = Encoding.UTF8; // TODO parameterize?
+            var bytes = encoding.GetBytes(data);
+            request.ContentLength = bytes.Length;
+
+            var tcs = new TaskCompletionSource<T>();
+            Spawn(Post(request, bytes, tcs, resultor), tcs);
+            return tcs.Task;
+        }
+
+        static IEnumerable<Task> Post<T>(WebRequest request, byte[] bytes, TaskCompletionSource<T> tcs, Func<WebHeaderCollection, string, T> resultor)
+        {
+            Debug.Assert(request != null);
+            Debug.Assert(bytes != null);
+            Debug.Assert(tcs != null);
+            Debug.Assert(resultor != null);
+
+            var getRequestStreamTask = request.GetRequestStreamAsync();
+            yield return getRequestStreamTask;
+
+            using (var stream = getRequestStreamTask.Result)
+            {
+                var writeTask = stream.WriteAsync(bytes, 0, bytes.Length);
+                yield return writeTask;
+            }
+
+            foreach (var task in Get(request, tcs, resultor))
+                yield return task;
         }
 
         public Task<T> Get<T>(WebHeaderCollection headers, Uri address, Func<WebHeaderCollection, string, T> resultor)
@@ -21,43 +74,80 @@ namespace Elmah.Io
             if (address == null) throw new ArgumentNullException("address");
             if (resultor == null) throw new ArgumentNullException("resultor");
 
-            return Transact(headers, webClient => webClient.DownloadStringAsTask(address), resultor);
+            var request = (HttpWebRequest) WebRequest.Create(address);
+            
+            if (headers != null)
+                request.Headers.Add(headers);
+
+            var tcs = new TaskCompletionSource<T>();
+            Spawn(Get(request, tcs, resultor), tcs);            
+            return tcs.Task;
         }
 
-        static Task<TResult> Transact<T, TResult>(NameValueCollection headers, Func<WebClient, Task<T>> transactor, Func<WebHeaderCollection, T, TResult> resultor)
+        static IEnumerable<Task> Get<T>(WebRequest request, TaskCompletionSource<T> tcs, Func<WebHeaderCollection, string, T> resultor)
         {
-            Debug.Assert(transactor != null);
+            Debug.Assert(request != null);
+            Debug.Assert(tcs != null);
             Debug.Assert(resultor != null);
 
-            var webClient = new WebClient();
-            IDisposable disposable = webClient;
-            try
+            var getResponseTask = request.GetResponseAsync();
+            yield return getResponseTask;
+
+            using (var response = getResponseTask.Result)
+            using (var stream = response.GetResponseStream())
             {
-                if (headers != null)
+                var contentType = response.Headers.Map(HttpResponseHeader.ContentType, h => new ContentType(h));
+                var encoding = contentType != null
+                    ? contentType.EncodingFromCharSet(Encoding.Default)
+                    : Encoding.Default;
+
+                var bytes = new byte[4096];
+                var chars = (char[]) null;
+                var decoder = encoding.GetDecoder();
+                var sb = new StringBuilder();
+                while (true)
                 {
-                    webClient.Headers.Add(headers);
+                    var readTask = stream.ReadAsync(bytes, 0, bytes.Length);
+                    yield return readTask;
+                    var readCount = readTask.Result;
+                    if (readCount == 0)
+                        break;
+                    var charCount = decoder.GetCharCount(bytes, 0, readCount);
+                    if (chars == null || charCount > chars.Length)
+                        chars = new char[charCount];
+                    var decodedCharCount = decoder.GetChars(bytes, 0, readCount, chars, 0, false);
+                    sb.Append(chars, 0, decodedCharCount);
                 }
 
-                var task = transactor(webClient).ContinueWith(t =>
+                tcs.SetResult(resultor(response.Headers, sb.ToString()));
+            }
+        }
+
+        static void Spawn<T>(IEnumerable<Task> job, TaskCompletionSource<T> tcs)
+        {
+            Debug.Assert(job != null);
+            Debug.Assert(tcs != null);
+
+            Task.Factory
+                .StartNew(job)
+                .ContinueWith(t =>
                 {
-                    try
+                    if (t.IsCanceled)
                     {
-                        return resultor(webClient.ResponseHeaders, t.Result);
+                        tcs.TrySetCanceled();
                     }
-                    finally 
+                    else if (t.IsFaulted)
                     {
-                        webClient.Dispose();
+                        var aggregate = t.Exception;
+                        Debug.Assert(aggregate != null);
+                        tcs.TrySetException(aggregate.InnerExceptions);
+                    }
+                    else
+                    {
+                        Debug.Assert(t.Status == TaskStatus.RanToCompletion);
                     }
                 },
                 TaskContinuationOptions.ExecuteSynchronously);
-
-                disposable = null;
-                return task;
-            }
-            finally
-            {
-                if (disposable != null) disposable.Dispose();
-            }
         }
     }
 }
---
+++ Elmah.Io/Mannex/Net/WebHeaderCollection.cs
@@ -0,0 +1,64 @@
+#region License, Terms and Author(s)
+//
+// Mannex - Extension methods for .NET
+// Copyright (c) 2009 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Mannex.Net
+{
+    #region Imports
+
+    using System;
+    using System.Net;
+
+    #endregion
+
+    /// <summary>
+    /// Extension methods for <see cref="WebHeaderCollection"/>.
+    /// </summary>
+
+    static partial class WebHeaderCollectionExtensions
+    {
+        /// <summary>
+        /// Applies a projection to response header if the response header 
+        /// is contained in the collection is non-empty. Otherwise it 
+        /// returns the default value for the type <typeparamref name="T"/>.
+        /// </summary>
+
+        public static T Map<T>(this WebHeaderCollection headers, HttpResponseHeader header, Func<string, T> mapper)
+        {
+            return Map(headers, header, default(T), mapper);
+        }
+
+        /// <summary>
+        /// Applies a projection to response header if the response header 
+        /// is contained in the collection is non-empty. Otherwise it 
+        /// returns a given default of type <typeparamref name="T"/>.
+        /// </summary>
+
+        public static T Map<T>(this WebHeaderCollection headers, HttpResponseHeader header, T defaultValue, Func<string, T> mapper)
+        {
+            if (headers == null) throw new ArgumentNullException("headers");
+            if (mapper == null) throw new ArgumentNullException("mapper");
+            var value = headers[header];
+            return string.IsNullOrEmpty(value) ? defaultValue : mapper(value);
+        }
+    }
+}
